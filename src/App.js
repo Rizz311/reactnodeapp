668c8380059268320e9caf4a8e2abb3f1f02fe2d
Rizz311 <stunna617@yahoo.com>
2018-08-01 13:36:20 -0400

    Made changes to the app.js file to fix a issue with app not comnpiling correctly.

--- src/App.js
+++ src/App.js
@@ -1,62 +1,77 @@
 import React, { Component } from 'react';
 import './App.css';
-
 class App extends Component {
+  getDBServer() {
+    return "http://localhost:3001";
+  }
+  render() {
+    let url = this.getDBServer() + "/locations";
+    return (
+      <div className="App"><LocationsList url={url}/></div>
+    )
+  }
+}
+class LocationsList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       rows: []
     };
   }
-
-  getDBServer() {
-    return "http://localhost:3001";
-  }
-
   componentDidMount() {
-    let locationurl = this.getDBServer() + "/locations";
-    let userurl = this.getDBServer() + "/users";
-    console.log("fetching " + locationurl);
-    console.log("fetching " + userurl);
-    fetch(locaiurl).then((response) => {
+    let url = this.props.url;
+    console.log("fetching (via props2)" + url);
+    fetch(url).then((response) => {
       response.json().then((data) => {
-        console.log("state from componentDidMount: " + this.state);
-        console.log("rows from componentDidMount: " + JSON.stringify(data));
-
-/////////////////////// *******************Difference between fatarrow and vanilla js**********************************///////////////////////
-           this.state.userRows({rows: data});
-           this.state.locationRows({rows: data})
-           console.log(data);
-        // this.setState({rows: data});
-        // this.setState({rows: data.map(function(x){
-        //   return x.name;
-        // })})
+        console.log("state1 from componentDidMount: " + this.state);
+        console.log("rows1 from componentDidMount: " + JSON.stringify(data));
+        this.setState({rows: data});
+        console.log("fetch1 done");
+                //  this.setState({rows: data.map(function(x) {return x.name})});
       }).catch(function(error) {
-        console.log("ERROR in fetch parse " + error);
+        console.log("e in fetch parse " + error);
       });
     }).catch(function(error) {
-      console.log("ERROR in fetch" + error);
+      console.log("e in fetch" + error);
+    });
+    fetch(url).then((response) => {
+      response.json().then((data) => {
+        console.log("state2 from componentDidMount: " + this.state);
+        console.log("rows2 from componentDidMount: " + JSON.stringify(data));
+        let s = this.state;
+        s.rows2 = data;
+        this.setState(s);
+        console.log("state2 again: " + JSON.stringify(s));
+        console.log("fetch2 done");
+                //  this.setState({rows: data.map(function(x) {return x.name})});
+      }).catch(function(error) {
+        console.log("e in fetch parse " + error);
+      });
+    }).catch(function(error) {
+      console.log("e in fetch" + error);
     });
   }
-
   render() {
-
     console.log("state from render: " + this.state);
     console.log("rows from render: " + JSON.stringify(this.state.rows));
-
-    let listItems = this.state.rows.map(loc => <li key={loc.name}>{loc.name}</li>);
+    let listItems = [];
+    if (this.state.rows) {
+      listItems = this.state.rows.map(loc => <li key={loc.name}>{loc.name}</li>);
+    }
+    let listItems2 = [];
+    if (this.state.rows2) {
+      listItems2 = this.state.rows2.map(loc => <li key={loc.name}>{loc.name}</li>);
+    }
     return (
-      <div>
-      <p className="locations">
-        {listItems}
-      </p>
-      <p className="users">
-        {listItems}
-      </p>
-      </div>
-
+        <div>
+        <ul className="Locations">
+          {listItems}
+        </ul>
+        <ul className="Locations">
+          {listItems2}
+        </ul>
+        </div>
     );
   }
 }
-
 export default App;
